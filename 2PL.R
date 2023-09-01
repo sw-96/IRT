50bad2006360e1f575e30b9ba7ef4ab4b138364e
sw-96 <95455785+sw-96@users.noreply.github.com>
2023-09-01 14:04:31 +0800

    Update 2PL.R

--- 2PL.R
+++ 2PL.R
@@ -4,4 +4,18 @@
 ## Menghitung durasi kalkulasi
 mulai <- Sys.time()
 
-## 
+## Library
+library(ltm)
+library(psych)
+library(readxl)
+library(writexl)
+
+## Membaca data input
+#data_mentah <- read.table("ouirt.dat", header = FALSE) # membaca masukan data, nama dan tipe file disesuaikan dengan kebutuhan.
+data_mentah <- read_excel(file.choose()) # mengambil file masukan berupa file excel
+nama_peserta_tes <- subset(data_mentah, select = Student) # membuat kolom berisi nama siswa.
+data_mentah <- subset(data_mentah, select = -c(Student)) # menghilangkan kolom nama peserta tes (student)
+
+## Statistik deskriptif
+data_deskriptif <- data.frame(describe(data_mentah))# membuatdata frame yang berisi data-data hasil pengolahan statistik deskriptif
+
